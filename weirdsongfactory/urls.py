573e7f847ffe2852d30e18c1cd3d3e26407ed3bc
sebbvu <pyamoto@protonmail.com>
2025-10-02 02:52:08 +0300

    fix audio serve in dev

--- weirdsongfactory/urls.py
+++ weirdsongfactory/urls.py
@@ -40,3 +40,4 @@ urlpatterns = [
 # Serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static('/assets/', document_root='assets')
